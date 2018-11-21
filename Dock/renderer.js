3f769abdc6732b9eb9525dfdc92175c0e6839a18
panzhiyi3 <panzhiyi3@gmail.com>
2018-11-21 20:14:09 +0800

    完成从dll读取路径的联调

--- Dock/renderer.js
+++ Dock/renderer.js
@@ -1,12 +1,23 @@
 window.$ = window.jQuery = require("./jquery.min");
-let win = require('electron').remote.getCurrentWindow();
-let chokidar = require('chokidar');
+let win = require("electron").remote.getCurrentWindow();
+let chokidar = require("chokidar");
+let ffi = require("ffi");
+let ref = require("ref");
+let path = require("path");
 
-$(".titlebar").children().css("opacity", "0.0");
+let currentPath = path.dirname(require.main.filename || process.mainModule.filename);
+console.log(currentPath);
+
+let libDimensionDesk = ffi.Library(currentPath + "\\DimensionDeskHelper64", {
+    "GetCurrentDesktopPath": [ "int", ["char *", "int", "int *"] ],
+    "GetAllUsersDesktopPath": [ "int", ["char *", "int", "int *"] ]
+});
 
 init();
 
 function init() {
+    $(".titlebar").children().css("opacity", "0.0");
+
     window.addEventListener('mousemove', function (e) {
         if (e.target === document.documentElement)
             win.setIgnoreMouseEvents(true, { forward: true });
@@ -53,6 +64,64 @@ function init() {
 
     $("#onMenuSettingClick").click(onMenuSettingClick);
     $("#onMenuCloseClick").click(onMenuCloseClick);
+
+
+    let paths = getDesktopPath();
+    if(!paths) {
+        console.log("Error getting system desktop path, exit!");
+        return;
+    }
+
+    var watcher = chokidar.watch(paths,
+        {
+            ignoreInitial: true,
+            depth: 0
+        });
+    watcher.on("add", function (path) {
+        console.log(path);
+    })
+}
+
+function getDesktopPath() {
+    let bufferLenth = 260 * 3;
+    let sizeNeed = ref.alloc("int");
+    let stringBuffer = new Buffer(bufferLenth).fill(0);
+    let currentPath = "";
+    let allUsersPath = "";
+
+    let callret = libDimensionDesk.GetCurrentDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    if(callret == -1)
+    {
+        bufferLenth = sizeNeed.deref();
+        stringBuffer = new Buffer(bufferLenth).fill(0);
+        callret = libDimensionDesk.GetCurrentDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    }
+    if(callret) {
+        currentPath = ref.readCString(stringBuffer); // Dll returns utf8 string
+        console.log(currentPath);
+    }
+    else {
+        return null;
+    }
+
+    stringBuffer.fill(0);
+
+    callret = libDimensionDesk.GetAllUsersDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    if(callret == -1)
+    {
+        bufferLenth = sizeNeed.deref();
+        stringBuffer = new Buffer(bufferLenth).fill(0);
+        callret = libDimensionDesk.GetAllUsersDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    }
+    if(callret) {
+        allUsersPath = ref.readCString(stringBuffer); // Dll returns utf8 string
+        console.log(allUsersPath);
+    }
+    else {
+        return null;
+    }
+
+    return [allUsersPath];
 }
 
 function onMenuSettingClick(e) {
