ea608a079397c60a4ff7dcc16c8cc3e130f86799
panzhiyi3 <panzhiyi3@gmail.com>
2018-11-22 18:35:52 +0800

    Get desktop files and shows

--- FoldDesktop/main.js
+++ FoldDesktop/main.js
@@ -1,36 +1,153 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const { ipcMain } = require('electron');
+let fs = require("fs");
+
+let ffi = require("ffi");
+let ref = require("ref");
+let path = require("path");
+let chokidar = require("chokidar");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+
+let libDimensionDesk;
 
 // https://github.com/electron/electron/blob/master/docs/api/frameless-window.md
 // https://github.com/electron/electron/issues/1335
 //app.disableHardwareAcceleration(); // This will enable mouse click through
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 800, height: 800, resizable: true, maximizable: false, frame: false, transparent: true })
-    var id = mainWindow.id
+    mainWindow = new BrowserWindow({ width: 800, height: 800, resizable: true, maximizable: false, frame: false, transparent: true });
+    let id = mainWindow.id;
 
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html');
 
-    mainWindow.webContents.openDevTools({mode: 'undocked'})
+    mainWindow.webContents.openDevTools({mode: 'undocked'});
 
     mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+        mainWindow = null;
+    });
+
+    mainWindow.webContents.on('did-finish-load', () => {
+        init(mainWindow);
+    });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 })
 
 app.on('activate', function () {
     if (mainWindow === null) {
-        createWindow()
+        createWindow();
     }
 })
+
+function init(mainWindow) {
+    let currentPath = path.dirname(require.main.filename || process.mainModule.filename);
+    
+    libDimensionDesk = ffi.Library(currentPath + "\\DimensionDeskHelper64", {
+        "GetCurrentDesktopPath": [ "int", ["char *", "int", "int *"] ],
+        "GetAllUsersDesktopPath": [ "int", ["char *", "int", "int *"] ]
+    });
+
+    let paths = getDesktopPath();
+    if(!paths) {
+        console.log("Error getting system desktop path, exit!");
+        return;
+    }
+
+    var watcher = chokidar.watch(paths,
+        {
+            ignoreInitial: true,
+            depth: 0
+        });
+    watcher.on("add", function (path) {
+        console.log(path);
+    });
+
+    let fileList = [];
+
+    for(let i = 0; i < paths.length; i++) {
+        traversePath(paths[i], fileList);
+    }
+
+    mainWindow.webContents.send('FileListReady', fileList);
+}
+
+function getDesktopPath() {
+    let bufferLenth = 260 * 3;
+    let sizeNeed = ref.alloc("int");
+    let stringBuffer = new Buffer(bufferLenth).fill(0);
+    let currentPath = "";
+    let allUsersPath = "";
+
+    let callret = libDimensionDesk.GetCurrentDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    if(callret == -1)
+    {
+        bufferLenth = sizeNeed.deref();
+        stringBuffer = new Buffer(bufferLenth).fill(0);
+        callret = libDimensionDesk.GetCurrentDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    }
+    if(callret) {
+        currentPath = ref.readCString(stringBuffer); // Dll returns utf8 string
+        console.log(currentPath);
+    }
+    else {
+        return null;
+    }
+
+    stringBuffer.fill(0);
+
+    callret = libDimensionDesk.GetAllUsersDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    if(callret == -1)
+    {
+        bufferLenth = sizeNeed.deref();
+        stringBuffer = new Buffer(bufferLenth).fill(0);
+        callret = libDimensionDesk.GetAllUsersDesktopPath(stringBuffer, bufferLenth, sizeNeed);
+    }
+    if(callret) {
+        allUsersPath = ref.readCString(stringBuffer); // Dll returns utf8 string
+        console.log(allUsersPath);
+    }
+    else {
+        return null;
+    }
+
+    return [currentPath, allUsersPath];
+}
+
+// get all files and folders
+function traversePath(filePath, fileList) {
+    let files = fs.readdirSync(filePath);
+    if (!files) {
+        console.warn(err);
+        return false;
+    }
+    else {
+        files.forEach(function (fileName) {
+            let thisFilePath = path.join(filePath, fileName);
+            let stats = fs.statSync(thisFilePath);
+
+            if (!stats) {
+                console.warn("fs.statSync failed on " + thisFilePath);
+            }
+            else {
+                let isFile = stats.isFile();
+                let isDir = stats.isDirectory();
+
+                let f = new Object();
+                f.path = thisFilePath;
+                f.name = fileName;
+                f.isDir = isDir;
+                fileList.push(f);
+            }
+        });
+    }
+    return true;
+}
--- FoldDesktop/renderer.js
+++ FoldDesktop/renderer.js
@@ -1,38 +1,34 @@
 window.$ = window.jQuery = require("./jquery.min");
-let win = require("electron").remote.getCurrentWindow();
-let fs = require("fs");
-let chokidar = require("chokidar");
-let ffi = require("ffi");
-let ref = require("ref");
-let path = require("path");
+let mainWin = require("electron").remote.getCurrentWindow();
+const {ipcRenderer} = require('electron');
 
-let currentPath = path.dirname(require.main.filename || process.mainModule.filename);
-console.log(currentPath);
-
-let libDimensionDesk = ffi.Library(currentPath + "\\DimensionDeskHelper64", {
-    "GetCurrentDesktopPath": [ "int", ["char *", "int", "int *"] ],
-    "GetAllUsersDesktopPath": [ "int", ["char *", "int", "int *"] ]
-});
+let globalFileList = [];
+let orgFileItemElem = $(".item").clone();
 
 init();
 
+ipcRenderer.on('FileListReady', (event, fileList) => {
+    onFileListReady(fileList);
+  });
+
 function init() {
+    $(".item").remove();
+
     $(".titlebar").children().css("opacity", "0.0");
 
+    // TODO, adjust element range
     window.addEventListener('mousemove', function (e) {
         if (e.target === document.documentElement)
-            win.setIgnoreMouseEvents(true, { forward: true });
+            mainWin.setIgnoreMouseEvents(true, { forward: true });
         else
-            win.setIgnoreMouseEvents(false);
+            mainWin.setIgnoreMouseEvents(false);
     });
 
     window.onresize = function () {
-        $(".background").height(document.documentElement.clientHeight);
-        $(".container").height(document.documentElement.clientHeight - $(".titlebar").height() - 10);
+        $(".background").height(document.documentElement.clientHeight - 11);
     }
 
-    $(".background").height(document.documentElement.clientHeight);
-    $(".container").height(document.documentElement.clientHeight - $(".titlebar").height() - 10);
+    $(".background").height(document.documentElement.clientHeight - 11);
 
     $(".sidebar-button").click(openSidebar);
 
@@ -71,67 +67,6 @@ function init() {
 
     $("#onMenuSettingClick").click(onMenuSettingClick);
     $("#onMenuCloseClick").click(onMenuCloseClick);
-
-
-    let paths = getDesktopPath();
-    if(!paths) {
-        console.log("Error getting system desktop path, exit!");
-        return;
-    }
-
-    var watcher = chokidar.watch(paths,
-        {
-            ignoreInitial: true,
-            depth: 0
-        });
-    watcher.on("add", function (path) {
-        console.log(path);
-    });
-
-    traversePath(paths[0]);
-    traversePath(paths[1]);
-}
-
-function getDesktopPath() {
-    let bufferLenth = 260 * 3;
-    let sizeNeed = ref.alloc("int");
-    let stringBuffer = new Buffer(bufferLenth).fill(0);
-    let currentPath = "";
-    let allUsersPath = "";
-
-    let callret = libDimensionDesk.GetCurrentDesktopPath(stringBuffer, bufferLenth, sizeNeed);
-    if(callret == -1)
-    {
-        bufferLenth = sizeNeed.deref();
-        stringBuffer = new Buffer(bufferLenth).fill(0);
-        callret = libDimensionDesk.GetCurrentDesktopPath(stringBuffer, bufferLenth, sizeNeed);
-    }
-    if(callret) {
-        currentPath = ref.readCString(stringBuffer); // Dll returns utf8 string
-        console.log(currentPath);
-    }
-    else {
-        return null;
-    }
-
-    stringBuffer.fill(0);
-
-    callret = libDimensionDesk.GetAllUsersDesktopPath(stringBuffer, bufferLenth, sizeNeed);
-    if(callret == -1)
-    {
-        bufferLenth = sizeNeed.deref();
-        stringBuffer = new Buffer(bufferLenth).fill(0);
-        callret = libDimensionDesk.GetAllUsersDesktopPath(stringBuffer, bufferLenth, sizeNeed);
-    }
-    if(callret) {
-        allUsersPath = ref.readCString(stringBuffer); // Dll returns utf8 string
-        console.log(allUsersPath);
-    }
-    else {
-        return null;
-    }
-
-    return [currentPath, allUsersPath];
 }
 
 function onMenuSettingClick(e) {
@@ -152,28 +87,17 @@ function closeSidebar() {
     $("#sideBar").removeClass("sidenav-expand");
 }
 
+function onFileListReady(list) {
+    globalFileList = list;
+    refreshFiles();
+}
 
-// get all files and folders
-function traversePath(filePath) {
-    fs.readdir(filePath, function(err, files) {
-        if(err) {
-            console.warn(err);
-            return null;
-        }
-        else {
-            files.forEach(function(filename) {
-                let thisFilePath = path.join(filePath, filename);
-                fs.stat(thisFilePath, function(eror, stats) {
-                    if(eror) {
-                        console.warn("fs.stat failed on " + thisFilePath);
-                    }
-                    else {
-                        let isFile = stats.isFile();
-                        let isDir = stats.isDirectory();
-                        console.log(thisFilePath);
-                    }
-                })
-            });
-        }
-    });
+function refreshFiles() {
+    for (let i = 0; i < globalFileList.length; i++) {
+        let elem = orgFileItemElem.clone();
+        elem.children(".filename").text(globalFileList[i].name);
+        console.log(globalFileList[i].name);
+
+        $(".container").append(elem);
+    }
 }
