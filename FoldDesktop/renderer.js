c82fe325166392838ee925181eb6366bf28667ef
panzhiyi3 <panzhiyi3@gmail.com>
2018-11-26 00:42:16 +0800

    Add fold button Try new hoverbar code(No good result now) Change filename style to correct text shadow

--- FoldDesktop/renderer.js
+++ FoldDesktop/renderer.js
@@ -6,6 +6,8 @@ let globalFileList = [];
 let globalFileListMap = new Map();
 let orgFileItemElem = $(".item").clone();
 
+let isFold = false;
+
 init();
 
 ipcRenderer.on('FileListReady', (event, fileList) => {
@@ -19,13 +21,25 @@ ipcRenderer.on('FileIconReady', (event, iconInfo) => {
 function init() {
     $(".item").remove();
 
-    // TODO, adjust element range
-    window.addEventListener('mousemove', function (e) {
-        if (e.target === document.documentElement)
-            mainWin.setIgnoreMouseEvents(true, { forward: true });
-        else
-            mainWin.setIgnoreMouseEvents(false);
-    });
+    // $(".background").mouseover(function (e) {
+    //     console.log("xixi");
+    //     mainWin.setIgnoreMouseEvents(true, { forward: true });
+    // }).mouseout(function (e) {
+    //     console.log("haha");
+    //     mainWin.setIgnoreMouseEvents(false);
+    // });
+    // window.addEventListener('mousemove', function (e) {
+    //     if(e.target) {
+    //         if(e.target.className == "background" || e.target.className == "container") {
+    //             mainWin.setIgnoreMouseEvents(true, { forward: true });
+    //             console.log("xixi");
+    //         }
+    //     }
+    //     else {
+    //         mainWin.setIgnoreMouseEvents(false);
+    //         console.log("haha");
+    //     }
+    // });
 
     window.onresize = function () {
         $(".background").height(document.documentElement.clientHeight - 11);
@@ -39,6 +53,8 @@ function init() {
 
     $(".sidebar-button").click(openSidebar);
 
+    $(".foldbutton").click(foldOrExpand);
+
     $("#sidebar-closebutton").click(closeSidebar);
 
     $("#sidebar-settingbutton").click(onSideBarSetting);
@@ -48,17 +64,19 @@ function init() {
 
 function mouseEnterHoverBar() {
     if(!$(".background").hasClass("background-hover")) {
-        $(".background").addClass("background-hover");
+        if(!isFold) {
+            $(".background").addClass("background-hover");
+        }
     }
-    $(this).css("opacity", "1.0");
+    //$(this).css("opacity", "1.0");
     $(".side-hoverbar").css("visibility", "visible");
 }
 
 function mouseLeaveHoverBar() {
-    if(!$("#sideBar").hasClass("sidenav-expand")) { // side bar opened
+    if(!$("#sideBar").hasClass("sidenav-expand")) { // side bar opened, don't show hover state
         $(".background").removeClass("background-hover");
     }
-    $(this).css("opacity", "0.0");
+    //$(this).css("opacity", "0.0");
     $(".side-hoverbar").css("visibility", "hidden");
 }
 
@@ -75,6 +93,28 @@ function closeSidebar() {
     $(".background").removeClass("background-hover");
     $("#sideBar").removeClass("sidenav-expand");
     mainWin.setResizable(false);
+}
+
+function foldOrExpand() {
+    if(isFold) {
+        isFold = false;
+        $(".container").fadeIn(50);
+        if($(".foldbutton").hasClass("fa-caret-right")) {
+            $(".foldbutton").removeClass("fa-caret-right")
+            .addClass("fa-caret-left")
+            .attr("title", "折叠");
+        }
+    }
+    else {
+        isFold = true;
+        $(".container").fadeOut(50);
+        $(".background").removeClass("background-hover");
+        if($(".foldbutton").hasClass("fa-caret-left")) {
+            $(".foldbutton").removeClass("fa-caret-left")
+            .addClass("fa-caret-right")
+            .attr("title", "展开");
+        }
+    }
 }
 
 function onSideBarSetting(e) {
@@ -111,7 +151,6 @@ function refreshFiles() {
         elem.attr("id", i);
         elem.children(".filename").text(globalFileList[i].name);
         elem.dblclick(runFileItem);
-
         $(".container").append(elem);
     }
 }
